a821ad23386f0ab3b8cf85965933ba9369399774
karnwalak <karnwalakshay7@gmail.com>
2022-09-17 13:34:36 +0530

    Na

--- public/js/datatable.js
+++
@@ -1,6 +0,0 @@
-// Call the dataTables jQuery plugin
-$(document).ready(function() {
-  $('#dataTable').DataTable();
-  $('#fileSidenav .table').DataTable();
-  $('#formSidenav .table').DataTable();
-});
